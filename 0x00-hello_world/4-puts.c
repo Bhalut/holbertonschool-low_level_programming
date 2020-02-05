ad7d8a139f5295804ed5633e8c5e4bdd103a92d9
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-05 21:27:50 +0000

    4-puts file 0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ *Main funtion display "Programming is like building a multilingual puzzle
+ */
+
+int main(void)
+{
+	puts(\"Programming is like building a multilingual puzzle);
+	return (0);
+}
