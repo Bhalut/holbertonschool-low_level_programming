ffc3ce891f99d20c0fda9bc9ba729b68c160752b
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-06 21:02:56 +0000

    Add 5-print_numbers.c file 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - display all single digit numbers of base 10 starting from 0
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (int = 0; i <= 9; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return (0);
+}
