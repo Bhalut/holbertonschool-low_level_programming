6222fe1972fa49d46dcdd252f6707b18822c88af
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-06 00:37:15 +0000

    101-quote.c file 0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
