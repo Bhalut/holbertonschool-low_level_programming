9bff7b8e1c5766e2481f65f9893494c0e125cfa4
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-06 00:32:25 +0000

    101-quote.c file 0x00-hello_world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - display and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *Return: 1
+ */
+
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
