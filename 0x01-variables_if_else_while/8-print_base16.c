9bbe645a68c746d680aab6f66542eede2f10bff4
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-06 21:45:22 +0000

    Add 8-print_base16.c file 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - display all the numbers of base 16 in lowercase
+ *Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i);
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
