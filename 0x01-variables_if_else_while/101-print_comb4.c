d19a18ff021716642ab32d10b9559da4e50e18df
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-06 23:15:26 +0000

    Add 101-print_comb4.c file 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ *main - display all possible different combinations of three digits
+ *Return: 0
+ */
+
+int main(void)
+{
+	int x;
+	int y = 0;
+	int z = 0;
+
+	for (x = 0; x <= 9; x++)
+	{
+		y = x + 1;
+		for (; y <= 9; y++)
+		{
+			z = y + 1;
+			for(; z <=9; z++)
+			{
+				putchar(x + '0');
+				putchar(y + '0');
+				putchar(z + '0');
+				if (!(x == 7 && y == 8 && z == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
