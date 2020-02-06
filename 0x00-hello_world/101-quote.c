c3dc345ca45aefa539e8a777201f52d04892d709
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-06 00:50:19 +0000

    101-quote.c file 0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  *main - display and that piece of art is useful" - Dora Korpar, 2015-10-19
@@ -7,6 +7,7 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
