3687d970ea10bf26dc21bbc4cb4ba640e3a4aee9
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-06 20:57:42 +0000

    Add 4-print_alphabt.c file 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - display the alphabet in lowercase, all the letters except q and e
+ *Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
