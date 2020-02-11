e61f1df81b9b5fe77301caf59191bc37eb0300a4
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-11 21:07:31 +0000

    10-add.c file 0x02-funtions_nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ *add - return add a x b
+ *@a: first parameter integer
+ *@b: second parameter integer
+ *Return: a x b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -7,6 +7,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int n);
 int print_last_digit(int n);
+int add(int, int);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 void jack_bauer(void);
