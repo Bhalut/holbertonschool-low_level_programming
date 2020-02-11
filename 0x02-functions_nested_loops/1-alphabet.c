5f2da1804f6215e0a1e121050346aae61f098007
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-11 16:56:40 +0000

    1-alphabet.c file 0x02-funtions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet - display alphabet
+ *Return: void
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
