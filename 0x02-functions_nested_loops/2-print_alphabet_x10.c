9e6cddcaa0ed0e0b64709d9ece37c62994d76b95
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-11 17:08:29 +0000

    2-print_alphabet_x10.c file 0x02-funtions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,14 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	char c;
 
 	for (i = 0; i <= 9; i++)
 	{
-		print_alphabet();
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
 	}
 }
