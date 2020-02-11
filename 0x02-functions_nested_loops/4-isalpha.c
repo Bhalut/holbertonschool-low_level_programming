782a2ab20a7be6b5409c4b9ddfca6a4ba076e0da
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-11 18:19:40 +0000

    4-isalpha.c file 0x02-funtions_nested_loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+int _isalpha(int c)
+{
+	int result;
+
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		result = 1;
+	}
+	else
+	{
+		result = 0;
+	}
+
+	return (result);
+}
