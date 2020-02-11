7c15b4fd6264d0f1fa2ea06252f2058fbf2eb384
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-11 22:17:36 +0000

    Add 0-main.c file 0x03-debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *main - display positive, negative or zero
+ *Return: 0 success
+ */
+
+int main (void)
+{
+	int i;
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
