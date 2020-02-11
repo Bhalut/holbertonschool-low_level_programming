9a30ea5175556c27dcef524dbd0ef6f3b893e8eb
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-11 17:03:47 +0000

    2-print_alphabet_x10.c file 0x02-funtions_nested_loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_x10 - display alphabet x10
+ *Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		print_alphabet();
+	}
+}
