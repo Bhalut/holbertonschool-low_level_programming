7c3a7f108c1ab4d8e09a969868ed4a41559e0ed6
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-11 22:23:01 +0000

    Add 0-main.c file 0x03-debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 
 /**
- *main - display positive, negative or zero
- *Return: 0 success
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main (void)
 {
 	int i;
-	i = 0;
 
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
