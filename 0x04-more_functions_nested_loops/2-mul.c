7f81548bcbbb8511fcde81b964fb3be4120bf8e6
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-13 19:13:21 +0000

    Add 2-mul.c file 0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,9 @@
 /**
- *
- *
- *
- */
+ *mul - multiple parameters
+ *@a: parameter enteger
+ *@b: parameter enteger
+ *Return: result of multiplication
+*/
 int mul(int a, int b)
 {
 	return (a * b);
