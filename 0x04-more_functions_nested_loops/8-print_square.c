09256722eebd7873dbcde0f0fa2c22a330ae3abc
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-13 21:41:06 +0000

    Add 8-print_square.c file 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ *print_square - print square with a size
+ *@size: parameter enteger
+ *Return: void
+ */
+void print_square(int size)
+{
+	int i, u;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (i = 0; i < size; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('#');
+	}
+}
