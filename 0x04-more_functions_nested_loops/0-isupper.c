12bcf1c654ce6cca60d54a98fc2bf422e3d0b870
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-13 17:37:36 +0000

    Add 0-isupper.c file 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+
+/**
+ *_isupper - return for uppercase
+ *@c: parameter integer
+ *Return: 1 or 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,6 @@
+#ifndef HOLBETON_H
+#define HOLBERTON_H
+
+int _isupper(int c);
+
+#endif /* HOLBERTON_H */
