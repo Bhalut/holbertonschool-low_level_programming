c87513cd88808032ecdb9242edb2cd5921c2e7a1
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-13 04:09:01 +0000

    add 3-print_remaining_days.c prototype in holberton.h

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -6,5 +6,7 @@
 void positive_or_negative(int i);
 void print_remaining_days(int month, int day, int year);
 int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* HOLBERTON_H */
