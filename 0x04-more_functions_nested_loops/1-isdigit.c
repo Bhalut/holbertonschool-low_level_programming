ca694e8b728208208a037c7b8f86a506b9db18f4
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-13 19:04:08 +0000

    Add 1-isdigit.c file 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+/**
+ *_isdigit - return 1 or 0 if is digit
+ *@c: parameter integer
+ *Return: 1 or 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
