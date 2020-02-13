30c034ebab24ae7f4c4ef767ab54726783e2ce04
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-13 19:32:50 +0000

    Add 4-print_most_numbers.c file 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+/**
+ *print_most_numbers - print numbers from 0 to 9, not 2 and 4
+ *Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -6,5 +6,6 @@ int _isdigit(int c);
 int _putchar(char c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
 
 #endif /* HOLBERTON_H */
