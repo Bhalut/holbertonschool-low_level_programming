3632cf8b3c3a048e80e168e618920774a5e345c4
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-18 19:19:35 +0000

    Add 1-swap.c file 0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,13 @@
 /**
- *
- *
+ *swap_int - swap form integer a to integer b
+ *@a: pointer integer
+ *@b: pointer integer
+ *Return: void
  */
 void swap_int(int *a, int *b)
 {
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
 }
