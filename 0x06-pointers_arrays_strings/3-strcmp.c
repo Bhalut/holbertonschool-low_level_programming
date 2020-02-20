a358229ad61295eec798cbfc912929b3814b4d5a
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-20 21:57:41 +0000

    Add 3-strncmp.c file 0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,33 @@
+/**
+ *_strcmp - compares two strings
+ *@s1: pointer parameter
+ *@s2: pointer parameter
+ *Return: n variable
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, n = 0, j = 0;
+
+	while (s1[i])
+		i++;
+
+	while (s2[j])
+		j++;
+
+	if (i == j)
+	{
+		n = 0;
+	}
+	else if (i < j)
+	{
+		n = i - j;
+		n = (n * 10) - i;
+	}
+	else
+	{
+		n = i - j;
+		n = (n * 10) + i - 1;
+	}
+
+	return (n);
+}
