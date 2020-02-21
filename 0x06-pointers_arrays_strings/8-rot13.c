188b1545f5b0259a3ca491efb33ce246a398684a
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-21 04:53:36 +0000

    Add 8-rot13.c file 0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -9,8 +9,8 @@ char *rot13(char *s)
 	char arr_char[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
 			   'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
 			   'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
-			   'e', 'f', 'g', 'h', 'i','j', 'k', 'l', 'm', 'n', 'o',
-			   'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+			   'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+			   'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
 			   'x', 'y', 'z'};
 	char arr_rot[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
 			  'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
@@ -20,7 +20,7 @@ char *rot13(char *s)
 
 	for (i = 0; s[i]; i++)
 	{
-		for(j = 0; arr_char[j]; j++)
+		for (j = 0; arr_char[j]; j++)
 		{
 			if (s[i] == arr_char[j])
 				s[i] = arr_rot[j];
