a0bd84fbfb186fc3f67d8ef73144b6e0f314ec59
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-22 01:01:07 +0000

    Add 8-rot13.c file 0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -5,25 +5,19 @@
  */
 char *rot13(char *s)
 {
-	int i, j;
-	char arr_char[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-			   'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-			   'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
-			   'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-			   'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
-			   'x', 'y', 'z'};
-	char arr_rot[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-			  'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-			  'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't',
-			  'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e',
-			  'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+	int i = 0, j = 0;
+	char *arr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *arr_rot = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i]; i++)
 	{
-		for (j = 0; arr_char[j]; j++)
+		for (j = 0; arr[j]; j++)
 		{
-			if (s[i] == arr_char[j])
+			if (s[i] == arr[j])
+			{
 				s[i] = arr_rot[j];
+				break;
+			}
 		}
 	}
 
