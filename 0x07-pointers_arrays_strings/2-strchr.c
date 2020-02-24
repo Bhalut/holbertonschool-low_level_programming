defe00359ae88ce6031e90eceea64a5c6d4bc92c
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-24 20:35:34 +0000

    add 2-strchr.c file 0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+/**
+ *_strchar - locates a character in a string.
+ *@s: pointer parameter char
+ *@c: parameter character
+ *Return: s variable
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == c)
+			s = &s[i];
+	}
+
+	return (s);
+}
