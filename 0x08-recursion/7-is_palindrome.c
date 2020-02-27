12738858a7a6dba0e37218e5782386b461774b50
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-27 04:56:11 +0000

    add 7-is_palindrome.c file 0x08-recursion

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,14 @@
+/**
+ *is_palindrome - returns 1 if a string is a palindrome and 0 if not.
+ *@s: string parameter
+ *Return: 1 or one
+ */
+int is_palindrome(char *s)
+{
+	if (*s)
+		return (1);
+	else if (*s == 0)
+		return (0);
+	else
+		return (is_palindrome);
+}
