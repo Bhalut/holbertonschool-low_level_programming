590cedce9a85c62b6d5dc6fc64e14c72cb3b7061
Abdel Mejia <bhalut.bear@gmail.com>
2020-02-27 04:45:31 +0000

    add 6-is_prime_number.c file 0x08-recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,25 @@
+/**
+ *cmp - comparation value
+ *@x: number to compare
+ *@y: value compare
+ *Return: result
+ */
+int cmp(int x, int y)
+{
+	if (x % y == 0)
+		return (1);
+	else (x % y > 0)
+		return (0);
+	else
+		return (cmp(x, y + 1));
+}
+
+/**
+ *is_prime_number - returns 1 if the input integer is a prime number.
+ *@n: input integer
+ *Return: 1 or 0
+ */
+int is_prime_number(int n)
+{
+	return (cmp(n, 1));
+}
