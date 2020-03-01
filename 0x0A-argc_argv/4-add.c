1c4b12f6b0425a19c620d15cac88de3f94d7d2d9
Abdel Mejia <bhalut.bear@gmail.com>
2020-03-01 01:26:01 +0000

    add 4-add.c file 0x0A-argc_argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -22,13 +22,12 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (isdigit(*argv[i]))
-				add += atoi(argv[i]);
-			else
+			if (!isdigit(*argv[i]))
 			{
 				printf("Error\n");
 				return (1);
 			}
+			add += atoi(argv[i]);
 		}
 		printf("%d\n", add);
 	}
