eb6eab014b3b7ac317394b8b6a578e3b940d32ef
Abdel Mejia <bhalut.bear@gmail.com>
2020-03-02 01:34:53 +0000

    add 100-atoi.c file 0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+/**
+ *_strcpy - convert a string to an integer.
+ *@s: pointer parameter char
+ *Return: integer value
+ */
+int _atoi(char *s)
+{
+	int i= 0, n = 0, aux = 1;
+	while (s[i])
+	{
+		if (s[i] == '-' && (s[i + 1] >= '0' && s[i + 1] <= '9'))
+		{
+			aux = -1;
+			break;
+		}
+		i++;
+	}
+
+	i = 0;
+
+	while (s[i])
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			n = (n * 10) - (s[i] - '0');
+
+			if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
+				break;
+		}
+		i++;
+	}
+
+	return (n * -aux);
+}
