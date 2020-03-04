3f7ac48a7097e51e4963ec8032b5784d8d22d988
Abdel Mejia <bhalut.bear@gmail.com>
2020-03-04 05:59:40 +0000

    add 5-argstostr.c file 0x0B-malloc_free

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,11 +1,10 @@
 #include <stdlib.h>
 
 /**
- *free_grid - frees a 2 dimensional grid previously
- *created by your alloc_grid function.
- *@grid: grid double pointer
- *@height: height grid
- *Return: void
+ *argstostr - concatenates all the arguments of your program.
+ *@ac: argc size double pointer
+ *@av: argv parameter double pointer
+ *Return: new strins by pointer
  */
 char *argstostr(int ac, char **av)
 {
