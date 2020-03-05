620e820325f2c27b8051062eb698613391668a58
Abdel Mejia <bhalut.bear@gmail.com>
2020-03-05 21:54:50 +0000

    Add 2-calloc.c file 0x0C-more_malloc_free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *ptr = malloc(size * nmemb);
 
-	if (nmemb == 0 && size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	if (ptr == NULL)
