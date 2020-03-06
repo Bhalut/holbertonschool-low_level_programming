0c3943436330bf68d23b338c4cfe9d3a94763103
Abdel Mejia <bhalut.bear@gmail.com>
2020-03-06 04:24:13 +0000

    add 3-array_range.c file 0x0C-more_malloc_free

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,5 +8,18 @@
  */
 int *array_range(int min, int max)
 {
-	
+	int *ptr;
+	int i;
+
+	if (min > max)
+		return (NULL);
+
+	ptr = malloc(sizeof(int) * (max - min) + 1);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i <= max; i++)
+		ptr[i] = min++;
+
+	return (ptr);
 }
