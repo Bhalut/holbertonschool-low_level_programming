135b4d7cf80745fe56390a613b4bbd83ef7032b3
Abdel Mejia <bhalut.bear@gmail.com>
2020-03-09 22:07:22 +0000

    Add 4-new_dog.c file 0x0E-structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,16 +12,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog = malloc(sizeof(dog_t));
 
-	if (dog)
-	{
-		dog->name = name;
-		dog->age = age;
-		dog->owner = owner;
-
-		return (dog);
-	}
-	else
-	{
+	if (dog == NULL)
 		return (NULL);
-	}
+
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+
+	return (dog);
 }
