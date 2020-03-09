c9b8119578f1008c38dd434a4744a6b942c59afb
Abdel Mejia <bhalut.bear@gmail.com>
2020-03-09 20:19:48 +0000

    Add 1-init_dog.c file 0x0E-structures_typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,7 @@
 #include "dog.h"
 
 /**
- *int_dog - initialize a variable of type struct dog
+ *init_dog - initialize a variable of type struct dog
  *@d: dog structure
  *@name: name init
  *@age: age init
