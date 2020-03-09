f7a842f5324bcc40f326b84aafca6c4942e3de50
Abdel Mejia <bhalut.bear@gmail.com>
2020-03-09 20:04:57 +0000

    Add dog.h file 0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - data of dogs
+ *@name: name of dog
+ *@age: age dog
+ *@owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
