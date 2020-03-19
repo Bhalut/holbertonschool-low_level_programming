22e2ea46b9fcc9ebab0eda13d3dd46ca4a2d885d
Abdel Mejia <bhalut.bear@gmail.com>
2020-03-19 21:41:57 +0000

    Add 2-add_node.c file 0x12-singly_linked_lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,14 +12,15 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newNode;
 
-	newNode = malloc(sizeof(list_t));
+	newNode = (list_t *)malloc(sizeof(list_t));
 	if (!newNode)
 		return (NULL);
 
 	newNode->str = strdup(str);
 	newNode->len = strlen(str);
-	newNode->next = *head;
+	newNode->next = (*head);
 
-	*head = newNode;
+	(*head) = newNode;
+
 	return (newNode);
 }
