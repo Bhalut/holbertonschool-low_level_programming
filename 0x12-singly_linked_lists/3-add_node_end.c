2b4c9269b589b376320ebff96ced67adf702fcfa
Abdel Mejia <bhalut.bear@gmail.com>
2020-03-19 22:56:23 +0000

    Add 3-add_node_end.c file 0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,50 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * _strlen - return size of array char
+ * @s: pointer integer
+ * Return: size of array char
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	return (i);
+}
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list.
+ * @head: linked lists
+ * @str: string to linked
+ * Return: new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newNode = (list_t *)malloc(sizeof(list_t));
+	list_t *lastNode = (*head);
+
+	if (!newNode)
+		return (NULL);
+
+	newNode->str = strdup(str);
+	newNode->len = _strlen(str);
+	newNode->next = NULL;
+
+	if (!(*head))
+	{
+		(*head) = newNode;
+		return (*head);
+	}
+
+	while (lastNode->next)
+		lastNode = lastNode->next;
+
+	lastNode->next = newNode;
+
+	return (newNode);
+}
