5f7dded1f3637f3757bfb4a48ead11c75dc99b34
Abdel Mejia <bhalut.bear@gmail.com>
2020-03-23 21:19:12 +0000

    add 0-print_listint.c file 0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,9 +2,9 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: linked list type listint_t
+ * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
