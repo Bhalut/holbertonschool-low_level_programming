0b14035d07c06a131cbaaeb94782ed129d1c5e95
Abdel Mejia <bhalut.bear@gmail.com>
2020-03-24 18:54:52 +0000

    add 10-delete_nodeint.c file 0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * _listint_len - returns the number of elements in a linked listint_t list.
+ * @h: linked list type listint_t
+ * Return: the number of nodes
+ */
+size_t _listint_len(const listint_t *h)
+{
+	unsigned int count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
+
+/**
+ * get_nodeint_at_index - the number of elements in a linked listint_t list.
+ * @head: linked lists
+ * @index: index to linked list
+ * Return: node by index
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *tmp = *head, *node;
+
+	if (index > _listint_len(*head))
+		return (-1);
+
+	if (tmp &&  index == 0)
+	{
+		*head = tmp->next;
+		free(tmp);
+		return (1);
+	}
+
+	while (tmp && i < index)
+	{
+		node = tmp;
+		tmp = tmp->next;
+		i++;
+	}
+
+	if (!tmp)
+		return (-1);
+
+	node->next = tmp->next;
+	free(tmp);
+
+	return (1);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -23,6 +23,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 int pop_listint(listint_t **head);
 int sum_listint(listint_t *head);
 
