28f77088df6c06f059d327842d475a0d45d15808
Abdel Mejia <bhalut.bear@gmail.com>
2020-03-27 03:09:14 +0000

    Add 1-print_binary file 0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: number in decimal
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int size = sizeof(unsigned long int);
+	unsigned long int max = 1 << (size * 7);
+	int isNumber = 0;
+
+	while (max)
+	{
+		if (!!(n&max))
+			isNumber = 1;
+
+		if (isNumber)
+			_putchar(!!(n&max) + '0');
+
+		max >>= 1;
+	}
+
+	if (!(isNumber))
+		_putchar('0');
+}
