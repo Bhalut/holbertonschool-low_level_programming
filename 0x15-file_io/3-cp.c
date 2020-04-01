8758211b5aa0e1da508ceec9db7d7f3019696924
Abdel Mejia <bhalut.bear@gmail.com>
2020-04-01 05:33:42 +0000

    Add 3-cp.c file 0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,26 +6,6 @@
 #include <unistd.h>
 
 /**
- *_strlen_recursion - returns the length of a string
- *@s: pointer parameter character
- *Return: count size
- */
-int _strlen_recursion(char *s)
-{
-	int count = 0;
-
-	if (*s)
-	{
-		count++;
-		return (count + _strlen_recursion(s + 1));
-	}
-	else
-	{
-		return (count);
-	}
-}
-
-/**
  * main - copy program
  * @argc: count paramenters
  * @argv: array to parameters
@@ -33,38 +13,41 @@ int _strlen_recursion(char *s)
  */
 int main(int argc, char *argv[])
 {
-	int file, r, w, c;
+	int file_one, file_two, r, w, c;
 	char buffer[1024];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
 			exit(97);
 
-	file = open(argv[1], O_RDONLY);
-	if (file == -1)
+	file_one = open(argv[1], O_RDONLY);
+	if (file_one == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 			argv[1]), exit(98);
 
-	r = read(file, buffer, 1024);
+	file_two = open(argv[2],  O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_two == -1)
+                dprintf(STDERR_FILENO, "Error: Can't write from file %s\n",
+                        argv[2]), exit(99);
+
+	r = read(file_one, buffer, 1024);
 	if (r == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 			argv[1]), exit(98);
-	c = close(file);
-	if (c == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file),
-			exit(100);
 
-	file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (file == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write from file %s\n",
-			argv[2]), exit(99);
-	w = write(file, buffer, _strlen_recursion(buffer));
+	w = write(file_two, buffer, r);
 	if (w == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write from file %s\n",
 			argv[2]), exit(99);
-	c = close(file);
+
+	c = close(file_one);
+        if (c == -1)
+                dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_one),
+                        exit(100);
+
+	c = close(file_two);
 	if (c == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file),
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_two),
 			exit(100);
 
 	return (0);
