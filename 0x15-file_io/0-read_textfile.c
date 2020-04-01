2cee3b4c5f63bc6acebd35db624a85f3f85d07b1
Abdel Mejia <bhalut.bear@gmail.com>
2020-04-01 02:46:41 +0000

    Add 0-read_textfile.c file 0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,42 +16,41 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int file, r, w, c;
 	char *buffer;
 
-	if (filename)
+	if (!filename)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+		return (0);
+
+	r = read(file, buffer, letters);
+	if (r == -1)
 	{
-		file = open(filename, O_RDONLY);
-		if (file == -1)
-			return (0);
-
-		buffer = malloc(sizeof(char) * letters);
-		if (!buffer)
-			return (0);
-
-		r = read(file, buffer, letters);
-		if (r == -1)
-		{
-			free(buffer);
-			return (0);
-		}
-
-		w = write(STDOUT_FILENO, buffer, r);
-		if (w == -1)
-		{
-			free(buffer);
-			return (0);
-		}
-
-		if (r != w)
-		{
-			free(buffer);
-			return (0);
-		}
-
 		free(buffer);
-		c = close(file);
-		if (c == -1)
-			return (0);
-
-		return (w);
+		return (0);
 	}
-	return (0);
+
+	w = write(STDOUT_FILENO, buffer, r);
+	if (w == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	if (r != w)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	c = close(file);
+	if (c == -1)
+		return (0);
+
+	return (w);
 }
