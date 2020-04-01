8dbae39bdd310926d8c72cdefba02f4960a72ec9
Abdel Mejia <bhalut.bear@gmail.com>
2020-04-01 05:52:38 +0000

    Add 3-cp.c file 0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,11 +25,13 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 			argv[1]), exit(98);
 
-	file_two = open(argv[2],  O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	file_two = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_two == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
 			argv[2]), exit(99);
 
+	for (r = 1024; r == 1024;)
+	{
 	r = read(file_one, buffer, 1024);
 	if (r == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
@@ -39,7 +41,7 @@ int main(int argc, char *argv[])
 	if (w == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
 			argv[2]), exit(99);
-
+	}
 	c = close(file_one);
 	if (c == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_one),
