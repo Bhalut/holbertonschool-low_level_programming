bdca15bff54e3f36598c69f725d66ba0b6b45135
Abdel Mejia <bhalut.bear@gmail.com>
2020-05-03 23:32:26 +0000

    Add add_dnodeint_end in lists.h file 0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -20,6 +20,7 @@ typedef struct dlistint_s
 #include <stdlib.h>
 #include <stdio.h>
 
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
