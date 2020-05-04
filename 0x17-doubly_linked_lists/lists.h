29c983e8bac362828febdbeabc916e50e983fd43
Abdel Mejia <bhalut.bear@gmail.com>
2020-05-04 04:13:21 +0000

    Add delete_dnodeint_at_index in lists.h file 0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -22,6 +22,7 @@ typedef struct dlistint_s
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 size_t print_dlistint(const dlistint_t *h);
