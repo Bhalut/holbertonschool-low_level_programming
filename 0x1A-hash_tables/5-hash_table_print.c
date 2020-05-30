3d9e36911d445f84b4adc5d8debbff52c96f351f
Abdel Mejia <bhalut.bear@gmail.com>
2020-05-30 04:04:42 +0000

    Add 5-hash_table_print.c file at 0x1A-hash_tables

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,5 +1,9 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_print - prints a hash table.
+ * @ht: the hash table
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned int index = 0, isOn = 0;
